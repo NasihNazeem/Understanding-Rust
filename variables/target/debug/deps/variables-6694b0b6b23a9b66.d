fac9088807431b4e18da753c764fcc19d5dedfc1
NasihNazeem <nasihnazeem@gmail.com>
2023-06-01 18:13:46 -0600

    created new cargo proj

---
+++ variables/target/debug/deps/variables-6694b0b6b23a9b66.d
@@ -0,0 +1,5 @@
+H:\Github\NasihNazeem\Understanding-Rust\variables\target\debug\deps\variables-6694b0b6b23a9b66.rmeta: src\main.rs
+
+H:\Github\NasihNazeem\Understanding-Rust\variables\target\debug\deps\variables-6694b0b6b23a9b66.d: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/deps/variables-8556b05192033b2e.d
@@ -0,0 +1,5 @@
+H:\Github\NasihNazeem\Understanding-Rust\variables\target\debug\deps\variables-8556b05192033b2e.rmeta: src\main.rs
+
+H:\Github\NasihNazeem\Understanding-Rust\variables\target\debug\deps\variables-8556b05192033b2e.d: src\main.rs
+
+src\main.rs:
